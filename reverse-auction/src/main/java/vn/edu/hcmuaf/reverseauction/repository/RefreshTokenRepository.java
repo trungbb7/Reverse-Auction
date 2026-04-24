2e934a8790c2268d11d277c4de8b4fff022f5443
Minh Trung Nguyen <trungbb7@gmail.com>
2026-04-24 21:27:16 +0700

    feat: update missing classes

---
+++ reverse-auction/src/main/java/vn/edu/hcmuaf/reverseauction/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package vn.edu.hcmuaf.reverseauction.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.edu.hcmuaf.reverseauction.entity.RefreshToken;
+import vn.edu.hcmuaf.reverseauction.entity.User;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    void deleteByUser(User user);
+}
---
+++ reverse-auction/src/main/java/vn/edu/hcmuaf/reverseauction/service/impl/RefreshTokenService.java
@@ -0,0 +1,47 @@
+package vn.edu.hcmuaf.reverseauction.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import vn.edu.hcmuaf.reverseauction.entity.RefreshToken;
+import vn.edu.hcmuaf.reverseauction.repository.RefreshTokenRepository;
+import vn.edu.hcmuaf.reverseauction.repository.UserRepository;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+
+    public RefreshToken createRefreshToken(Long userId) {
+        RefreshToken refreshToken = RefreshToken.builder()
+                .user(userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found")))
+                .token(UUID.randomUUID().toString())
+                .expiryDate(Instant.now().plusMillis(1000L * 60 * 60 * 24 * 7)) // 7 days
+                .build();
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshTokenRepository.findByToken(token);
+    }
+
+    public RefreshToken verifyExpiration(RefreshToken token) {
+        if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
+            refreshTokenRepository.delete(token);
+            throw new RuntimeException(token.getToken() + " Refresh token was expired. Please make a new signin request");
+        }
+        return token;
+    }
+
+    @Transactional
+    public void deleteByUserId(Long userId) {
+        userRepository.findById(userId).ifPresent(refreshTokenRepository::deleteByUser);
+    }
+}
